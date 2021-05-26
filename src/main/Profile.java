3bc50e5fd30cb4bd450a3c4070de22508e7b5955
Youssef A. Elgazar <62875086+Youssef-Elgazar@users.noreply.github.com>
2021-05-26 22:46:16 +0200

    Finished Profile class.

--- src/main/Profile.java
+++ src/main/Profile.java
@@ -1,5 +1,67 @@
 package main;
+import java.util.*;
 
 public class Profile {
+	private String gender;
+	private int age;
+	private String userName;
+	private String biography;
+	private String education;
+	private String work;
+	private String relationshipStatus;
+	
+	public Profile(String gender, int age, String userName) {
+		 this.gender = gender;
+		 this.age = age;
+		 this.userName = userName;
+	}
+	
+	public void setBiography(String bio) {
+		this.biography = bio;
+	}
+	
+	public void setEducation(String edu) {
+		this.education = edu;
+	}
+	
+	public void setWork(String work) {
+		this.work = work;
+	}
+	
+	public void setRelationshipStatus(String relationStatus) {
+		this.relationshipStatus = relationStatus;
+	}
+	
+	/* =========================== */
+	
+	public String getGender() {
+		return this.gender;
+	}
+	
+	public int getAge() {
+		return this.age;
+	}
+	
+	public String getBio() {
+		return this.biography;
+	}
+	
+	public String getEdu() {
+		return this.education;
+	}
+	
+	public String getWork() {
+		return this.work;
+	}
+	
+	public String getRS() {
+		return this.relationshipStatus;
+	}
+	
+	public String getUserName() {
+		return this.userName;
+	}
+	
 
+	
 }
--- src/main/User.java
+++ src/main/User.java
@@ -3,7 +3,6 @@ package main;
 public class User {
 
 	public static void main(String[] args) {
-		
 
 	}
 
