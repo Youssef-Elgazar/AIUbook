8dd158733f0bab9e15d0ace3f99fe8fc4262f129
Youssef A. Elgazar <62875086+Youssef-Elgazar@users.noreply.github.com>
2021-05-27 00:07:35 +0200

    Finished Post class. Has some issues.

--- src/main/Feed.java
+++ src/main/Feed.java
@@ -1,5 +1,6 @@
 package main;
+import java.util.*;
 
 public class Feed {
-
+	
 }
--- src/main/Post.java
+++ src/main/Post.java
@@ -1,5 +1,35 @@
 package main;
+import java.util.*;
 
 public class Post {
+	private User postAuthor;
+	private String postContent;
+	private int numOfPostLikes;
+	private int numOfComments;
+	private ArrayList<String> postComments  = new ArrayList<String>();
+	private String privacy;
+	
+	Post(String postContent, String Privacy, User postCreator){
+		this.postContent = postContent;
+		this.privacy = privacy;
+		this.postAuthor = postCreator;
+	}
+	
+	public void comment(String newComment, Profile commenter) {
+		this.postComments.add(commenter.getUserName() + "\n" + newComment);
+		this.numOfComments++;
+	}
+	
+	public void like() {
+		this.numOfPostLikes++;
+		
+		// ISSUE: Same post can be liked by the same user multiple times.
+	}
+	
+	public void share() {
+		// ISSUE: Can't keep track of original poster and the sharer.
+		// ISSUE#2: Can't make new post because can't call a constructor in a method.
+	}
 
+	
 }
