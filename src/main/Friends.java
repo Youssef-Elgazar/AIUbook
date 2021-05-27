7ef5a5a81fcd29b762a13151a856d6d871cba727
MuhammadSoliman14 <84283979+MuhammadSoliman14@users.noreply.github.com>
2021-05-27 13:33:31 +0200

    Update Friends.java

--- src/main/Friends.java
+++ src/main/Friends.java
@@ -1,5 +1,35 @@
 package main;
 
+import java.util.ArrayList;
+
 public class Friends {
+	private ArrayList <UserAccount> friends = new ArrayList <UserAccount>();  
+	private ArrayList <UserAccount> following = new ArrayList <UserAccount>();  
+	
+	public void addFriend(UserAccount newFriend) {
+		friends.add(newFriend);
+	}
+	
+	public void removeFriend(UserAccount removeUser) {
+		friends.remove(removeUser);
+		
+	}
+	
+	
+	// View friend list.
+	public void view() {
+		friends.toString();
+	}
+	
+	public void follow(UserAccount followUser) {
+		following.add(followUser);
+	}
+	
+	public static void main(String[] args) {
+		
+
+	}
+
+}
 
 }
